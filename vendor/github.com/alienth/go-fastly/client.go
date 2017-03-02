e393b1d225a96ac5ab1d2bc1a02d4ff933fa4253
Jason Harvey <alienth@gmail.com>
2017-03-01 21:53:37 -0900

    Update dep.

--- vendor/github.com/alienth/go-fastly/client.go
+++ vendor/github.com/alienth/go-fastly/client.go
@@ -9,11 +9,20 @@ import (
 	"net/http"
 	"net/url"
 	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
 )
 
-// defaultBaseURL is the default endpoint for Fastly. Since Fastly does not
-// support an on-premise solution, this is likely to always be the default.
-const defaultBaseURL = "https://api.fastly.com/"
+const (
+	// defaultBaseURL is the default endpoint for Fastly. Since Fastly does not
+	// support an on-premise solution, this is likely to always be the default.
+	defaultBaseURL = "https://api.fastly.com/"
+
+	headerRateLimitRemaining = "Fastly-RateLimit-Remaining"
+	headerRateLimitReset     = "Fastly-RateLimit-Reset"
+)
 
 // ProjectURL is the url for this library.
 var ProjectURL = "github.com/alienth/go-fastly"
@@ -60,6 +69,14 @@ type Client struct {
 	VCL            *VCLConfig
 	// apiKey is the Fastly API key to authenticate requests.
 	apiKey string
+
+	rateMu    sync.Mutex
+	rateLimit Rate
+}
+
+type Rate struct {
+	Remaining int
+	Reset     time.Time
 }
 
 type config struct {
@@ -147,7 +164,14 @@ func (c *Client) NewJSONRequest(method, urlStr string, body interface{}) (*http.
 // Do sends an API request and returns the response. The response is JSON
 // decoded and stored in the value pointed to by v, or returned as an error if
 // an API error has occurred.
+// If rate limit is exceeded and reset time is in the future, Do returns
+// *RateLimitError immediately without making a network API call.
 func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
+	// If we've hit rate limit, don't make further requests before Reset time.
+	if err := c.checkRateLimitBeforeDo(req); err != nil {
+		return nil, err
+	}
+
 	resp, err := c.client.Do(req)
 	if err != nil {
 		return nil, err
@@ -158,6 +182,13 @@ func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
 		io.CopyN(ioutil.Discard, resp.Body, 512)
 		resp.Body.Close()
 	}()
+
+	rate := parseRate(resp)
+	if rate != (Rate{}) {
+		c.rateMu.Lock()
+		c.rateLimit = rate
+		c.rateMu.Unlock()
+	}
 
 	err = CheckResponse(resp)
 	if err != nil {
@@ -178,6 +209,7 @@ func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
 // CheckResponse takes in an HTTP response containing a JSON-encoded error,
 // unmarshals the error, and returns it. Assumes no error if status code is
 // successful.
+// The error type will be *RateLimitError for rate limit exceeded errors,
 func CheckResponse(r *http.Response) error {
 	if c := r.StatusCode; 200 <= c && c <= 299 {
 		return nil
@@ -188,6 +220,14 @@ func CheckResponse(r *http.Response) error {
 		json.Unmarshal(data, errorResponse)
 	}
 
+	if c := r.StatusCode; c == 429 {
+		return &RateLimitError{
+			Rate:     parseRate(r),
+			Response: errorResponse.Response,
+			Message:  errorResponse.Message,
+		}
+	}
+
 	// 401 Unauthorized
 	// {"msg":"Provided credentials are missing or invalid"}
 	// 400 Bad Request
@@ -196,6 +236,80 @@ func CheckResponse(r *http.Response) error {
 	// {"msg":"You are not authorized to perform this action"}
 
 	return errorResponse
+}
+
+func parseRate(resp *http.Response) Rate {
+	var rate Rate
+	if remaining := resp.Header.Get(headerRateLimitRemaining); remaining != "" {
+		rate.Remaining, _ = strconv.Atoi(remaining)
+	}
+
+	if reset := resp.Header.Get(headerRateLimitReset); reset != "" {
+		if v, _ := strconv.ParseInt(reset, 10, 64); v != 0 {
+			rate.Reset = time.Unix(v, 0)
+		}
+	}
+
+	return rate
+}
+
+// checkRateLimitBeforeDo does not make any network calls, but uses existing knowledge from
+// current client state in order to quickly check if *RateLimitError can be immediately returned
+// from Client.Do, and if so, returns it so that Client.Do can skip making a network API call unnecessarily.
+// Otherwise it returns nil, and Client.Do should proceed normally.
+func (c *Client) checkRateLimitBeforeDo(req *http.Request) error {
+	// GETs and HEADs are not ratelimited
+	if req.Method == "GET" || req.Method == "HEAD" {
+		return nil
+	}
+	c.rateMu.Lock()
+	rate := c.rateLimit
+	c.rateMu.Unlock()
+	if !rate.Reset.IsZero() && rate.Remaining == 0 && time.Now().Before(rate.Reset) {
+		// Create a fake response.
+		resp := &http.Response{
+			Status:     http.StatusText(http.StatusForbidden),
+			StatusCode: http.StatusForbidden,
+			Request:    req,
+			Header:     make(http.Header),
+			Body:       ioutil.NopCloser(strings.NewReader("")),
+		}
+		return &RateLimitError{
+			Rate:     rate,
+			Response: resp,
+			Message:  fmt.Sprintf("API rate limit still exceeded until %v, not making remote request.", rate.Reset),
+		}
+	}
+
+	return nil
+}
+
+// RateLimitError occurs when Fastly returns 403 Forbidden response with a rate limit
+// remaining value of 0, and error message starts with "API rate limit exceeded for ".
+type RateLimitError struct {
+	Rate     Rate           // Rate specifies last known rate limit for the client
+	Response *http.Response // HTTP response that caused this error
+	Message  string         `json:"message"` // error message
+}
+
+func (r *RateLimitError) Error() string {
+	return fmt.Sprintf("%v %v: %d %v; rate reset in %v",
+		r.Response.Request.Method, r.Response.Request.URL,
+		r.Response.StatusCode, r.Message, r.Rate.Reset.Sub(time.Now()))
+}
+
+// RateLimits returns the rate limit for the current client. If a ratelimit
+// response has yet to be seen, returns nil.
+func (c *Client) RateLimit() *Rate {
+	c.rateMu.Lock()
+	rate := c.rateLimit
+	c.rateMu.Unlock()
+
+	if rate == (Rate{}) {
+		return nil
+	}
+
+	return &rate
 }
 
 // ErrorResponse represents the error message sent back from Fastly.
